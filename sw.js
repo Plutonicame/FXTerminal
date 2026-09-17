cbb0529853f2ff1e47c9d0f45c9bcb7be835483a
app <paulolasko@gmail.com>
2026-09-17 07:11:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * ce sera ajouté plus tard si besoin (ex. cache des réponses API).
  */
 
-const CACHE_VERSION = 'fx-bias-shell-v5';
+const CACHE_VERSION = 'fx-bias-shell-v6';
 
 const APP_SHELL = [
   './',
