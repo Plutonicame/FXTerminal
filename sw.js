0301bb0f2c5ade74e015a231218183097058317c
app <paulolasko@gmail.com>
2026-09-20 23:06:19 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * ce sera ajouté plus tard si besoin (ex. cache des réponses API).
  */
 
-const CACHE_VERSION = 'fx-terminal-shell-v38';
+const CACHE_VERSION = 'fx-terminal-shell-v39';
 
 const APP_SHELL = [
   './',
